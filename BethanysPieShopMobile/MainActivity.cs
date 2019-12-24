ffd35991b50f0d4f7ecbc6aec6db2bac77368641
safemarkMBuchoff <mbuchoff@safemark.com>
2019-12-24 16:55:55 -0500

    Demo - Creating the first screen

---
+++ BethanysPieShopMobile/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace BethanysPieShopMobile
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        private string value;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button myButton = FindViewById<Button>(Resource.Id.MyButton);
+            myButton.Click += MyButton_Click;
+
+        }
+
+        private void MyButton_Click(object sender, System.EventArgs e)
+        {
+            var toast = Toast.MakeText(this, "A button was clicked", ToastLength.Short);
+            toast.Show();
+
+            var intent = new Intent(this, typeof(SecondActivity));
+            StartActivity(intent);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            value = "OnPause";
+
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            value = "OnStart";
+
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            value = "OnResume";
+
+        }
+    }
+}
---
+++ BethanysPieShopMobile/PieDetailActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BethanysPieShopMobile.Core.Model;
+using BethanysPieShopMobile.Core.Repository;
+using BethanysPieShopMobile.Utility;
+
+namespace BethanysPieShopMobile
+{
+    [Activity(Label = "PieDetailActivity", MainLauncher = true)]
+    public class PieDetailActivity : Activity
+    {
+        private PieRepository _pieRepository;
+        private Pie _selectedPie;
+        private ImageView _pieImageView;
+        private TextView _pieNameTextView;
+        private TextView _shortDescriptionTextView;
+        private TextView _descriptionTextView;
+        private TextView _priceTextView;
+        private EditText _amountEditText;
+        private Button _addToCardButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.PieDetail);
+            // Create your application here
+            _pieRepository = new PieRepository();
+            _selectedPie = _pieRepository.GetPieById(1);
+            FindViews();
+            BindData();
+        }
+
+        private void BindData()
+        {
+            _pieNameTextView.Text = _selectedPie.Name;
+            _shortDescriptionTextView.Text = _selectedPie.ShortDescription;
+            _descriptionTextView.Text = _selectedPie.LongDescription;
+            _priceTextView.Text = "Price: " + _selectedPie.Price;
+
+            var imageBitmap = ImageHelper.GetImageBitmapFromUrl(_selectedPie.ImageThumbnailUrl);
+
+            _pieImageView.SetImageBitmap(imageBitmap);
+        }
+
+        private void FindViews()
+        {
+            _pieImageView = FindViewById<ImageView>(Resource.Id.pieImageView);
+            _pieNameTextView = FindViewById<TextView>(Resource.Id.pieNameTextView);
+            _shortDescriptionTextView = FindViewById<TextView>(Resource.Id.shortDescriptionTextView);
+            _descriptionTextView = FindViewById<TextView>(Resource.Id.descriptionTextView);
+            _priceTextView = FindViewById<TextView>(Resource.Id.priceTextView);
+            _amountEditText = FindViewById<EditText>(Resource.Id.amountEditText);
+            _addToCardButton = FindViewById<Button>(Resource.Id.addToCartButton);
+        }
+    }
+}
